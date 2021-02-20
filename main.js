5b50f4307a190fcbd514f9b33763178e1d2e0420
maarcf <marr.cairo@gmail.com>
2021-02-20 11:59:58 -0300

    add some styles and comic fetch

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
 const baseUrl = 'https://gateway.marvel.com/v1/public/';
 const apiKey = '08b7060939db82c5ed50966d57a02ac5';
 
+const mainSection = document.querySelector('#results');
+const totalResults = document.querySelector('#total-results');
+console.log(totalResults);
+
+const drawComics = () => {
+  fetch(`${baseUrl}/comics?apikey=${apiKey}&offset=0&orderBy=title`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    let comics = data.data.results;
+    let total = data.data.total;
+
+    
+    totalResults.textContent = `${total} RESULTADOS`
+    mainSection.innerHTML = '';
+    comics.map(comic => {
+      mainSection.innerHTML += `
+      <article class="comic-card">
+        <div class="comic-img-container">
+          <img class="comic-img" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.title}" />
+        </div>
+        <h3 class="comic-title">${comic.title}</h3>
+      </article>`
+    })
+
+  })
+}
+
+drawComics()
