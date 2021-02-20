be5e2684350c16cc275a02d11532e2a509f3094b
maarcf <marr.cairo@gmail.com>
2021-02-20 20:34:03 -0300

    pagination style, loader accessibility

--- main.js
+++ main.js
@@ -2,17 +2,37 @@
 const mainSection = document.querySelector('#results');
 const totalResults = document.querySelector('#total-results');
 const loaderOverlay = document.querySelector('.overlay');
-
-
+const firstPageButton = document.querySelector('#first-page');
+const previousPageButton = document.querySelector('#previous-page');
+const nextPageButton = document.querySelector('#next-page');
+const lastPageButton = document.querySelector('#last-page');
 
 // JS Variables//
 const baseUrl = 'https://gateway.marvel.com/v1/public/';
 const apiKey = '08b7060939db82c5ed50966d57a02ac5';
 
 // Loader //
-const showLoader = () => loaderOverlay.classList.remove('hidden');
-const hideLoader = () => loaderOverlay.classList.add('hidden');
+const showLoader = () => {
+  loaderOverlay.children[0].setAttribute('aria-hidden', 'false');
+  loaderOverlay.classList.remove('hidden');
+  mainSection.setAttribute('aria-busy', 'true')
+}
+const hideLoader = () => {
+  loaderOverlay.children[0].setAttribute('aria-hidden', 'true');
+  loaderOverlay.classList.add('hidden');
+  mainSection.setAttribute('aria-busy', 'false')
+}
 
+// Disabled and Enabled //
+const isDisabled = button => {
+  button.disabled = true;
+  button.children[0].style.color = '#757575';
+}
+
+const isEnabled = button => {
+  button.disabled = false;
+  button.children[0].style.color = '#fff';
+}
 
 const drawComics = () => {
   fetch(`${baseUrl}/comics?apikey=${apiKey}&offset=0&orderBy=title`)
@@ -28,15 +48,16 @@ const drawComics = () => {
     mainSection.innerHTML = '';
     comics.map(comic => {
       mainSection.innerHTML += `
-      <article class="comic-card">
+      <article class="comic-card" data-comic-id="${comic.id}">
         <div class="comic-img-container">
           <img class="comic-img" src="${comic.thumbnail.path}.${comic.thumbnail.extension}" alt="${comic.title}" />
         </div>
         <h3 class="comic-title">${comic.title}</h3>
       </article>`
     })
-    hideLoader()
-    
+    hideLoader();
+    isDisabled(firstPageButton);
+    isDisabled(previousPageButton);
   })
 }
 
